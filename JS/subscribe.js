243cf60df403209ec432269f9ae07cac9cbef991
Zura328 <andreijose328@gmail.com>
2021-11-18 16:36:51 +0800

    eyyo

---
+++ JS/subscribe.js
@@ -0,0 +1,7 @@
+function subscribe(){
+	if (document.getElementById('email').value==""){
+	alert("Please fill up the form");
+	}else{
+	alert("Thank you for Subscribing!!! We will update you as new product is available");
+	}
+}
